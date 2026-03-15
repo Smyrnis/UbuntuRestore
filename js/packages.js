ec26b1c6814bd0ee7ebddffa57868b39fa7436e4
Giannis Smyrnis <smyrnisgiannis@gmail.com>
2026-03-15 18:54:00 +0200

    update Vs code download

--- js/packages.js
+++ js/packages.js
@@ -494,9 +494,7 @@ const EDITORS = [
   {
     value: 'VS Code', label: 'VS Code',
     lines: [
-      'curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/packages.microsoft.gpg',
-      'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list',
-      'sudo apt update && sudo apt install -y code',
+      'sudo snap install code',
     ],
   },
   {
